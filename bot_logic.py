2b66afe579bd8f71093bdbf326af6f172be68699
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-03-11 21:39:40 +0200

    arhiv

--- bot_logic.py
+++ bot_logic.py
@@ -439,7 +439,7 @@ class ProjektySvodka(BaseMiddleware):
                     prodvizenije=[]
                     for i in range (2,12):
                         prodvizenije.append(next_row[2*i+1])
-                    for i in range (2,10):
+                    for i in range (2,12):
                         projekt_predstv_etapov.append(next_row[2*i])
                     svjaz.append(prodvizenije)
                     pokazatel_uspecha=next_row[2*2+1]*1+next_row[3*2+1]*2+next_row[4*2+1]*3+next_row[5*2+1]*4+next_row[6*2+1]*5+next_row[7*2+1]*6
@@ -1121,8 +1121,8 @@ async def projekt_arhiv_3(message: types.Message,state: FSMContext):
         await state.clear()
     if proverka_5 == 1 and proverka_4 == 1 and proverka_3 == 1:
         projekt_v_arhiv[0]=id_proverka_arhiv
-        projekt_eksempljar = Проект_Архив(id=projekt_v_arhiv[0], Название_Проекта=projekt_v_arhiv[0], Критерий_Завершенности=projekt_v_arhiv[1],
-        Этап_1=projekt_v_arhiv[2], Этап_2=projekt_v_arhiv[3], Этап_3=projekt_v_arhiv[4], Этап_4=projekt_v_arhiv[5], Этап_5=projekt_v_arhiv[6],
+        projekt_eksempljar = Проект_Архив(id=projekt_v_arhiv[0], Название_Проекта=projekt_v_arhiv[1], Критерий_Завершенности=projekt_v_arhiv[2],
+        Этап_1=projekt_v_arhiv[3], Этап_2=projekt_v_arhiv[4], Этап_3=projekt_v_arhiv[5], Этап_4=projekt_v_arhiv[5], Этап_5=projekt_v_arhiv[6],
         Этап_6=projekt_v_arhiv[7], Этап_7=projekt_v_arhiv[8], Этап_8=projekt_v_arhiv[9], Этап_9=projekt_v_arhiv[10], Этап_10=projekt_v_arhiv[11],
                                   Дата_внесения=projekt_v_arhiv[12], Синхронизация=projekt_v_arhiv[13])
         session = session_factory()
