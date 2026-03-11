cf202bb297d9639956ad1e6ec5997b1f1e745b44
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-03-11 22:00:43 +0200

    arhiv

--- bot_logic.py
+++ bot_logic.py
@@ -74,7 +74,7 @@ proverka_3=0
 proverka_4=0
 proverka_5=0
 naydennost=0
-id_proverka_arhiv=0
+id_proverka_arhiv=100
 #работа с базой данных
 from sqlalchemy import  DateTime, String, Float, Column, Integer, func, Text, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
